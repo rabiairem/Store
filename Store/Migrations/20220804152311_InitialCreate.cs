8c854901979149360beac426c35f0e0432462627
Rabia Irem Ucar <rabia-irem.ucar@capgemini.com>
2022-08-04 17:23:47 +0200

    migrations added

---
+++ Store/Migrations/20220804152311_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreServiceAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stores",
+                columns: table => new
+                {
+                    SapNumber = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Abbreviation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SmsStoreNumber = table.Column<int>(type: "int", nullable: false),
+                    IsFranchise = table.Column<bool>(type: "bit", nullable: false),
+                    PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Province = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FlowersModule = table.Column<int>(type: "int", nullable: false),
+                    IsOpenOnSunday = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stores", x => x.SapNumber);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stores");
+        }
+    }
+}
