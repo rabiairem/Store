c158a4a4d0c60518790d8fcfb24088f5cd610604
Rabia Irem Ucar <rabia-irem.ucar@capgemini.com>
2022-08-04 15:28:33 +0200

    storerepository added

---
+++ Store/Repository/IStoreRepository.cs
@@ -0,0 +1,13 @@
+﻿using StoreServiceAPI.Models;
+
+namespace StoreServiceAPI.Repository
+{
+    public interface IStoreRepository
+    {
+        Task<IEnumerable<StoreDTO>> GetStores();
+        Task<StoreDTO> GetStoreById(int storeId);
+        Task<StoreDTO> CreateStore(StoreDTO storeDto);
+        Task<StoreDTO> UpdateStore(StoreDTO storeDto);
+        Task<bool> DeleteStore(int storeId);
+    }
+}
---
+++ Store/Repository/StoreRepository.cs
@@ -0,0 +1,32 @@
+﻿using StoreServiceAPI.Models;
+
+namespace StoreServiceAPI.Repository
+{
+    public class StoreRepository : IStoreRepository
+    {
+        public Task<StoreDTO> CreateStore(StoreDTO storeDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteStore(int storeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<StoreDTO> GetStoreById(int storeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<StoreDTO>> GetStores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<StoreDTO> UpdateStore(StoreDTO storeDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
