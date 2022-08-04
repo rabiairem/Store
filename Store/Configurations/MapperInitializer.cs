db901235c8746537429a603f44a066c650435df3
Rabia Irem Ucar <rabia-irem.ucar@capgemini.com>
2022-08-04 15:22:56 +0200

    mapper initializer and nuget packages added

---
+++ Store/Configurations/MapperInitializer.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using StoreServiceAPI.Models;
+using StoreServiceAPI.Entities;
+
+namespace StoreServiceAPI.Configurations
+{
+    public class MapperInitializer : Profile
+    {
+        public MapperInitializer()
+        {
+            CreateMap<Store, StoreDTO>().ReverseMap();
+        }
+    }
+}
