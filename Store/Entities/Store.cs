cbede9c18ce1c5c537de9ee646ca07c12e44b876
Rabia Irem Ucar <rabia-irem.ucar@capgemini.com>
2022-08-04 15:22:32 +0200

    entity and model added

---
+++ Store/Entities/Store.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreServiceAPI.Entities
+{
+    public class Store
+    {
+        [Required]
+        [Range(1, 500)]
+        public string Name { get; set; }
+
+        [Key]
+        public int SapNumber { get; set; }
+
+        public string Abbreviation { get; set; }
+
+        public int SmsStoreNumber { get; set; }
+
+        public bool IsFranchise { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Province { get; set; }
+        public int FlowersModule { get; set; }
+
+        public bool IsOpenOnSunday { get; set; }
+    }
+}
---
+++ Store/Models/StoreDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreServiceAPI.Models
+{
+    public class StoreDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 500, ErrorMessage = "Store Name Is Too Long")]
+        public string Name { get; set; }
+
+        [Required]
+        public int SapNumber { get; set; }
+
+        public string Abbreviation { get; set; }
+
+        public int SmsStoreNumber { get; set; }
+
+        public bool IsFranchise { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+
+        public string Province { get; set; }
+        public int FlowersModule { get; set; }
+
+        public bool IsOpenOnSunday { get; set; }
+    }
+}
